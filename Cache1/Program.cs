2996043203e38e96ff3040b61490a8b0165aaf85
Martin <martin_lenart@icloud.com>
2026-01-08 11:07:06 +0100

    update

---
+++ Cache1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Cache1.Services;  
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Cache!");
+
+var service = new PrimeNumberService();
+var timer = new Stopwatch();
+
+timer.Start();
+var result = await service.GetPrimesCountAsync(2, 10_000_000);
+result += await service.GetPrimesCountAsync(10_000_002, 20_000_000);
+timer.Stop();
+Console.WriteLine($"First call took: {timer.ElapsedMilliseconds} ms and found {result} prime numbers.");
+
+timer.Restart();
+result = await service.GetPrimesCountAsync(2, 10_000_000);
+result += await service.GetPrimesCountAsync(10_000_002, 20_000_000);
+timer.Stop();
+Console.WriteLine($"Second call took: {timer.ElapsedMilliseconds} ms and found {result} prime numbers.");
+
+timer.Restart();
+result = await service.GetPrimesCountAsync(2, 10_000_000);
+timer.Stop();
+Console.WriteLine($"Third call took: {timer.ElapsedMilliseconds} ms and found {result} prime numbers.");
---
+++ Cache1/Services/PrimeNumberService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using Cache1.Models;
+using System.Collections.Concurrent;
+using Cache1.Extensions;
+
+namespace Cache1.Services
+{
+    public class PrimeNumberService
+    { 
+        ConcurrentDictionary<string, PrimeBatch> _primeNumberCache = new ();
+
+        public async Task<int> GetPrimesCountAsync(int start, int count)
+        {
+            PrimeBatch pResponse = null;
+            var key = new CacheKey(start, count);
+
+            //Check if Cache already contains the value
+            if (!_primeNumberCache.TryGetValue(key.ToString(), out pResponse))              
+            {
+                //If not, calculate the value
+                var nrPrimes = await NrPrimesInRangeAsync(start, count);
+                pResponse = new PrimeBatch(start, count, nrPrimes);
+
+                //Store the calculated value in the Cache
+                _primeNumberCache.TryAdd(key.ToString(), pResponse);
+
+                //Persist the Cache to disk
+                _primeNumberCache.SerializeJson("PrimeCache.json");
+            }
+
+            return pResponse.NrPrimes;
+        }
+
+        private Task<int> NrPrimesInRangeAsync(int start, int count) => Task.Run(() => NrPrimesInRange(start, count));
+        private int NrPrimesInRange(int start, int count) => 
+            Enumerable.Range(start, count).Count(n =>
+                Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0));
+
+
+        public PrimeNumberService()
+        {
+            try
+            {
+                _primeNumberCache = _primeNumberCache.DeSerializeJson("PrimeCache.json");
+            }
+            catch (Exception ex)
+            {
+                _primeNumberCache = new ();
+            }
+        }
+    }
+}
